1826b00282b795fb53a5750dbdc468750dbe1889
caselongo <caselongo@gmail.com>
2021-01-07 18:16:35 +0100

    z

--- youtube.go
+++ youtube.go
@@ -1,70 +1,31 @@
 package Youtube
 
 import (
-	"net/http"
-
-	bigquerytools "github.com/leapforce-libraries/go_bigquerytools"
-	errortools "github.com/leapforce-libraries/go_errortools"
-	oauth2 "github.com/leapforce-libraries/go_oauth2"
+	google "github.com/leapforce-libraries/go_google"
 )
 
 const (
-	apiName         string = "Youtube"
-	apiURL          string = "https://youtubeanalytics.googleapis.com/v2"
-	authURL         string = "https://accounts.google.com/o/oauth2/v2/auth"
-	tokenURL        string = "https://oauth2.googleapis.com/token"
-	tokenHTTPMethod string = http.MethodPost
-	redirectURL     string = "http://localhost:8080/oauth/redirect"
+	APIName string = "Youtube"
+	APIURL  string = "https://youtubeanalytics.googleapis.com/v2"
 )
 
 // Youtube stores Youtube configuration
 //
 type Youtube struct {
-	oAuth2 *oauth2.OAuth2
+	Client *google.GoogleClient
 }
 
 // methods
 //
-func NewYoutube(clientID string, clientSecret string, scope string, bigQuery *bigquerytools.BigQuery) *Youtube {
-	yt := Youtube{}
-	config := oauth2.OAuth2Config{
-		APIName:         apiName,
-		ClientID:        clientID,
-		ClientSecret:    clientSecret,
-		Scope:           scope,
-		RedirectURL:     redirectURL,
-		AuthURL:         authURL,
-		TokenURL:        tokenURL,
-		TokenHTTPMethod: tokenHTTPMethod,
-	}
-	yt.oAuth2 = oauth2.NewOAuth(config, bigQuery)
-	return &yt
-}
-
-func (yt *Youtube) ValidateToken() (*oauth2.Token, *errortools.Error) {
-	return yt.oAuth2.ValidateToken()
-}
-
-func (yt *Youtube) InitToken() *errortools.Error {
-	return yt.oAuth2.InitToken()
-}
-
-func (yt *Youtube) Get(url string, model interface{}) (*http.Response, *errortools.Error) {
-	_, res, e := yt.oAuth2.Get(url, model, nil)
-
-	if e != nil {
-		return nil, e
+func NewYoutube(clientID string, clientSecret string, scope string, bigQuery *google.BigQuery) *Youtube {
+	config := google.GoogleClientConfig{
+		APIName:      APIName,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scope:        scope,
 	}
 
-	return res, nil
-}
+	googleClient := google.NewGoogleClient(config, bigQuery)
 
-func (yt *Youtube) Patch(url string, model interface{}) (*http.Response, *errortools.Error) {
-	_, res, e := yt.oAuth2.Patch(url, nil, model, nil)
-
-	if e != nil {
-		return nil, e
-	}
-
-	return res, nil
+	return &Youtube{googleClient}
 }
