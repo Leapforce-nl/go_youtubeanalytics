1115ab8df2ab878fab3cf753a90ee532e1262e81
caselongo <caselongo@gmail.com>
2021-04-20 23:59:30 +0200

    x

--- Service.go
+++ Service.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	APIName string = "Youtube"
-	APIURL  string = "https://youtubeanalytics.googleapis.com/v2"
+	apiName string = "Youtube"
+	apiURL  string = "https://youtubeanalytics.googleapis.com/v2"
 )
 
 // Service stores Service configuration
@@ -17,19 +17,37 @@ type Service struct {
 	googleService *google.Service
 }
 
+type ServiceConfig struct {
+	ClientID     string
+	ClientSecret string
+	Scope        string
+}
+
 // methods
 //
-func NewService(clientID string, clientSecret string, scope string, bigQueryService *bigquery.Service) *Service {
+func NewService(serviceConfig *ServiceConfig, bigQueryService *bigquery.Service) (*Service, *errortools.Error) {
+	if serviceConfig == nil {
+		return nil, errortools.ErrorMessage("ServiceConfig must not be a nil pointer")
+	}
+
+	if serviceConfig.ClientID == "" {
+		return nil, errortools.ErrorMessage("ClientID not provided")
+	}
+
+	if serviceConfig.ClientSecret == "" {
+		return nil, errortools.ErrorMessage("ClientSecret not provided")
+	}
+
 	config := google.ServiceConfig{
-		APIName:      APIName,
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		Scope:        scope,
+		APIName:      apiName,
+		ClientID:     serviceConfig.ClientID,
+		ClientSecret: serviceConfig.ClientSecret,
+		Scope:        serviceConfig.Scope,
 	}
 
 	googleService := google.NewService(config, bigQueryService)
 
-	return &Service{googleService}
+	return &Service{googleService}, nil
 }
 
 func (service *Service) InitToken() *errortools.Error {
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/leapforce-libraries/go_youtube
 go 1.15
 
 require (
-	github.com/leapforce-libraries/go_errortools v0.0.0-20210113165137-bcbf687ce842
-	github.com/leapforce-libraries/go_google v0.0.0-20210219145550-4c15aa6a5e71
+	github.com/leapforce-libraries/go_errortools v0.0.0-20210419202804-48861072a520
+	github.com/leapforce-libraries/go_google v0.0.0-20210414170124-9343570001e8
 )
